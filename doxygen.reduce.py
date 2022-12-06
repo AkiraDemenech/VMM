6b0f709262750053f87ec77155313614ea73efd4
AkiraDemenech <guiakira.demenechmori@gmail.com>
2022-12-06 00:07:13 -0300

    Largura da linha

--- doxygen.reduce.py
+++ doxygen.reduce.py
@@ -15,9 +15,10 @@ with open('doconfig', 'r', encoding='utf-8') as dox:
 		dados.append([col.strip() for col in ln.split('=') if not col.isspace()])	
 		
 with open('doxygen_config', 'w', encoding='utf-8') as docs:
-	print('#--------------------------------------------------------------------------- \n# Project related configuration options \n#--------------------------------------------------------------------------- \n', file=docs)
+	
 	
 	comprimento = 4
+	tamanho = 0
 	for d in dados:
 		if len(d):
 			config[d[0]] = d[1:]
@@ -25,6 +26,15 @@ with open('doxygen_config', 'w', encoding='utf-8') as docs:
 			
 			if len(d[0]) > comprimento:
 				comprimento = len(d[0])
+			t = - len(d[0]) + 2
+			for c in d:
+				t += len(c) + 1
+			if t > tamanho:	
+				tamanho = t
+				
+	print('#','-' * (tamanho + comprimento), ' ', file=docs)			
+	print('# Project related configuration options ', file=docs)
+	print('#','-' * (tamanho + comprimento), ' ', file=docs)			
 				
 	for n in nomes:			
 		print(n,' ' * (comprimento - len(n)), '=', *config[n], file=docs)
